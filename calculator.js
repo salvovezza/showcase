dee2480170f6c14233d9914cde657ab3613f724d
salvovezza <155612782+salvovezza@users.noreply.github.com>
2024-03-02 17:15:42 +0000

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,11 +1,29 @@
-let calculation = '';
-      function mainCalculation(strParameter) {
+let calculation = '';      
+
+    updateScoreElement();  
+
+      function mainCalculation(strParameter) {        
         calculation += strParameter;
         
-        console.log(calculation);
+        document.querySelector('.total-display').innerHTML = calculation;               
       }
-     
-     function calculateEqual (calculation) {
-      document.querySelector('.js-calculation').innerHTML = calculation;
-      calculation = 0;
-     }       
+
+      function displayCalculation(calculation) {
+        if (calculation !== '') {
+           document.querySelector('.total-display-next').innerHTML = ` = ${calculation}`;
+           localStorage.setItem('calculation2', JSON.stringify(calculation)); 
+           document.querySelector('.yes-display').innerHTML = `Calculation is: ${calculation}`; 
+           console.log('here');
+        }     
+      } 
+
+      function updateScoreElement() {   
+        let calculation3 = JSON.parse(localStorage.getItem('calculation2'));       
+
+        if (calculation3 === null) {
+          calculation3 = 0;
+          document.querySelector('.yes-display').innerHTML = `Calculation is: ${calculation3}`;
+        }
+
+        document.querySelector('.yes-display').innerHTML = `Calculation is: ${calculation3}`;         
+      }      
